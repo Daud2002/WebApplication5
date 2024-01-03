f2a76bf50dd16ef28cfcf845e4d41d3c0ad75dcd
Daud2002 <daudmir3@gmail.com>
2024-01-03 18:42:53 +0500

    task

---
+++ WebApplication5/Controllers/Courser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+       
+        List<int> marks = new List<int> { 60, 45, 75, 30, 55, 90 };
+
+       
+        var selectedMarks = from mark in marks
+                            where mark > 50 && mark >= 85
+                            select mark;
+
+        
+        Console.WriteLine("Marks more than 50 and 85 plus:");
+        foreach (var mark in selectedMarks)
+        {
+            Console.WriteLine(mark);
+        }
+    }
+}
---
+++ WebApplication5/Controllers/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication5.Controllers
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
